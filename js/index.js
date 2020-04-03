4965523ec03744efc76f2ec726387086f2b99762
markandmarkup <markeyles88@gmail.com>
2020-04-03 02:47:57 +0100

    Portfolio section complete. Added js to display sidebar numbers.

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const sidebars = document.querySelectorAll('.side-numbers');
+
+var sideNumberStart = 4;
+var fontSize = parseInt(window.getComputedStyle(document.querySelector('html')).fontSize);
+
+sidebars.forEach((sidebar, index)=>{
+    if (index > 0) {
+        let parentHeight = sidebar.parentElement.clientHeight - 6;
+        let noOfLines = parentHeight / (fontSize * 1.5);
+        for(let i = 0; i < noOfLines; i++) {
+            sidebar.innerHTML += Math.floor(i + sideNumberStart);
+            sidebar.innerHTML += '<br>';
+        }
+    }
+
+});
