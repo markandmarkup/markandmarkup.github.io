265ddb0d630c8db7c94721a7a8f6dab70139ce19
markandmarkup <markeyles88@gmail.com>
2020-04-04 15:21:17 +0100

    Added basic About section

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
-const sidebars = document.querySelectorAll('.side-numbers');
+window.addEventListener('load', ()=>{
 
-var sideNumberStart = 4;
-var fontSize = parseInt(window.getComputedStyle(document.querySelector('html')).fontSize);
+    const sidebars = document.querySelectorAll('.side-numbers');
 
-sidebars.forEach((sidebar, index)=>{
-    if (index > 0) {
-        let parentHeight = sidebar.parentElement.clientHeight - 6;
-        let noOfLines = parentHeight / (fontSize * 1.5);
-        for(let i = 0; i < noOfLines; i++) {
-            sidebar.innerHTML += Math.floor(i + sideNumberStart);
-            sidebar.innerHTML += '<br>';
+    var sideNumberStart = 4;
+    var fontSize = parseInt(window.getComputedStyle(document.querySelector('html')).fontSize);
+
+    sidebars.forEach((sidebar, index)=>{
+        if (index > 0) {
+            let parentHeight = sidebar.parentElement.clientHeight - 6;
+            let noOfLines = parentHeight / (fontSize * 1.5);
+            for(let i = 0; i < noOfLines; i++) {
+                sidebar.innerHTML += Math.floor(i + sideNumberStart);
+                sidebar.innerHTML += '<br>';
+            }
+            sideNumberStart = sideNumberStart + noOfLines;
         }
-    }
+    });
 
 });
