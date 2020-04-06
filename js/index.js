ba2aa395d8674caa7e2a053e30347430d73c400c
markandmarkup <markeyles88@gmail.com>
2020-04-06 17:22:49 +0100

    Added styles for mobile display

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
+const sidebars = document.querySelectorAll('.side-numbers');
+const socialLinks = document.querySelectorAll('.social-link a');
+
 window.addEventListener('load', ()=>{
+    addSocialLinkUnderline(socialLinks);
+    generateSideBarNumbers(sidebars);
 
-    const sidebars = document.querySelectorAll('.side-numbers');
-    const socialLinks = document.querySelectorAll('.social-link a')
+    if (window.innerWidth <= 500) {
+        resizeHeader();
+    }
+});
 
-    var sideNumberStart = 4;
-    var fontSize = parseInt(window.getComputedStyle(document.querySelector('html')).fontSize);
+window.addEventListener('resize', ()=>{
+    if (window.innerWidth <= 500) {
+        resizeHeader();
+        generateSideBarNumbers(sidebars);
+    }
+});
 
+function addSocialLinkUnderline(socialLinks) {
     socialLinks.forEach((socialLink)=>{
         socialLink.addEventListener('mouseover', (e)=>{
             socialLink.children[1].style.textDecoration = 'underline';
@@ -17,9 +29,15 @@ window.addEventListener('load', ()=>{
             socialLink.children[1].style.textDecoration = 'none';
         })
     });
+}
+
+function generateSideBarNumbers(sidebars) {
+    let sideNumberStart = 4;
+    let fontSize = parseInt(window.getComputedStyle(document.querySelector('html')).fontSize);
 
     sidebars.forEach((sidebar, index)=>{
         if (index > 0) {
+            sidebar.innerHTML = '';
             let parentHeight = sidebar.parentElement.clientHeight - 6;
             let noOfLines = parentHeight / (fontSize * 1.5);
             for(let i = 0; i < noOfLines; i++) {
@@ -29,5 +47,10 @@ window.addEventListener('load', ()=>{
             sideNumberStart = sideNumberStart + noOfLines;
         }
     });
+}
 
-});
+function resizeHeader() {
+    let topLogo = document.getElementsByClassName('top-logo')
+    let logoHeight = parseInt(window.getComputedStyle(topLogo[0]).height);
+    topLogo[0].nextElementSibling.style.fontSize = (Math.floor(logoHeight) + 2) + 'px';
+}
